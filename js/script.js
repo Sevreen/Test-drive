19844ddf54f76cf4f16a542eec23cbc5f55d7344
Владислав <Sevreen73@gmail.com>
2016-11-08 14:25:33 +0300

    Done

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var link = document.querySelector(".btn-call-me");
+var overlay = document.querySelector(".modal-overlay");
+
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  overlay.classList.add("modal-content-show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  overlay.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+      overlay.classList.remove("modal-content-show");
+    }
+  }
+});
